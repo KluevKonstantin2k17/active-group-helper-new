2e4cf5c00ea8bf2b0752ada2ffc0ef491065dd4a
KluevKonstantin2k17 <54535343+KluevKonstantin2k17@users.noreply.github.com>
2025-06-20 18:30:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"  # Замени на переменную окружения
+#BOT_TOKEN = "YOUR_BOT_TOKEN"  # Замени на переменную окружения
 USER_ADMINS = [123456789]      # Твой Telegram ID (админ)
 CHAT_OK = -1001234567890       # Чат для ОК
 CHAT_RADIO = -1009876543210    # Чат для радистов
